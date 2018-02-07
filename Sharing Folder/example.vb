08dd4c6299bbb024a3c477ff0d302560fba88b86
jeffcheng <b98502008@gmail.com>
2018-02-07 09:34:27 +0800

    Create example.vb

---
+++ Sharing Folder/example.vb
@@ -0,0 +1,22 @@
+'Create a list that will hold our permissions
+Dim PermissionsList As New List(Of SharePermissionEntry)
+
+'Create a new permission entry for the Everyone group and specify that we want to allow them Read access
+Dim PermEveryone As New SharePermissionEntry(String.Empty, "Everyone", SharedFolder.SharePermissions.Read, True)
+'Create a new permission entry for the currently logged on user and specify that we want to allow them Full Control
+Dim PermUser As New SharePermissionEntry(Environment.UserDomainName, Environment.UserName, SharedFolder.SharePermissions.FullControl, True)
+ 
+'Add the two entries declared above to our list
+PermissionsList.Add(PermUser)
+PermissionsList.Add(PermEveryone)
+ 
+'Share the folder as "Test Share" and pass in the desired permissions list
+Dim Result As SharedFolder.NET_API_STATUS = _
+SharedFolder.ShareExistingFolder("Test Share", "This is a test share", "C:\SomeFolder", PermissionsList)
+ 
+'Show the result
+If Result = SharedFolder.NET_API_STATUS.NERR_Success Then
+    MessageBox.Show("Share created successfully!")
+Else
+    MessageBox.Show("Share was not created as the following error was returned: " & Result.ToString)
+End If
